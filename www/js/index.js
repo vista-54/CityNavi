125bc9cf3641fcba27d5907c4b7c17d4ff83bd4c
vista-54 <168aa3c72>
2015-07-24 21:47:53 +0300

    24.07

--- www/js/index.js
+++ www/js/index.js
@@ -9,42 +9,42 @@ var store = window.localStorage;
 function StartScanBeacons() {
     initapp();
 }
-//========================MENU======================================
-var time = 800;
-var easing = 'swing';
-function openMenu() {
-    $('#slide-menu').stop().show().animate({width: '80%'}, time, easing);
-}
+////========================MENU======================================
+//var time = 800;
+//var easing = 'swing';
+//function openMenu() {
+//    $('#slide-menu').stop().show().animate({width: '80%'}, time, easing);
+//}
+//
+//function closeMenu() {
+//    $('#slide-menu').stop().animate({width: '0'}, time, easing, function () {
+//        $('#slide-menu').hide();
+//    });
+//}
+//window.addEventListener("message", receiveMessage, false);
 
-function closeMenu() {
-    $('#slide-menu').stop().animate({width: '0'}, time, easing, function () {
-        $('#slide-menu').hide();
-    });
-}
-window.addEventListener("message", receiveMessage, false);
-
-function receiveMessage(event)
-{
-
-    var urlToOpen = event.data;
-    var obj = JSON.parse(event.data);
-    for (var i in obj) {
-        var action = i;
-        var val = obj[i];
-        if (action == 'open') {
-            openDocument(val);
-        }
-        if (action == 'swipe') {
-            if (val == 'left') {
-                openMenu();
-            }
-            if (val == 'right') {
-                closeMenu();
-            }
-        }
-    }
-
-}
+//function receiveMessage(event)
+//{
+//
+//    var urlToOpen = event.data;
+//    var obj = JSON.parse(event.data);
+//    for (var i in obj) {
+//        var action = i;
+//        var val = obj[i];
+//        if (action == 'open') {
+//            openDocument(val);
+//        }
+//        if (action == 'swipe') {
+//            if (val == 'left') {
+//                openMenu();
+//            }
+//            if (val == 'right') {
+//                closeMenu();
+//            }
+//        }
+//    }
+//
+//}
 function toolAction() {
     if ($('#slide-menu').is(":visible")) {
         closeMenu();
@@ -67,34 +67,34 @@ $(document).ready(function () {
 function addFirstStartToStrore(status) {
     store.setItem('FirstStart', JSON.stringify(status));
 }
-function setDataID() {
-    $.ajax({
-        type: "POST",
-        url: "ajax.php",
-        dataType: 'JSON',
-        data: {
-            uuid: firstStartApp,
-        },
-        beforesend: $('.content').html('Загрузка'),
-        success: function (data, code) {
-            if (code == 200) {
-                $('.content').html(data);
-            } else {
-                $('.content').html(code);
-            }
-
-            $('.content').html('Данные которые вернул сервер ' + data.fio[1]); // данные которые вернул сервер!
-
-        },
-        error: function (xhr, str) {
-            $('.content').html('Критическая ошибка');
-        },
-        complete: function () {
-            // $('#something').hide();
-        }
-
-    });
-}
+//function setDataID() {
+//    $.ajax({
+//        type: "POST",
+//        url: "ajax.php",
+//        dataType: 'JSON',
+//        data: {
+//            uuid: firstStartApp,
+//        },
+//        beforesend: $('.content').html('Загрузка'),
+//        success: function (data, code) {
+//            if (code == 200) {
+//                $('.content').html(data);
+//            } else {
+//                $('.content').html(code);
+//            }
+//
+//            $('.content').html('Данные которые вернул сервер ' + data.fio[1]); // данные которые вернул сервер!
+//
+//        },
+//        error: function (xhr, str) {
+//            $('.content').html('Критическая ошибка');
+//        },
+//        complete: function () {
+//            // $('#something').hide();
+//        }
+//
+//    });
+//}
 //===============================FIRST START APP END=====================
 //======================================authentication===================
 
@@ -113,17 +113,17 @@ function getDeviceInfo() {
 }
 //======================================authentication END===============
 
-$(function () {
-    $("#slide-menu").swipe({
-        //Generic swipe handler for all directions
-        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
-            //$(this).text("You swiped " + direction ); 
-            console.log("You swiped " + direction);
-            if (direction == "left")
-            {
-                closeMenu();
-            }
-
-        }
-    });
-});
+//$(function () {
+//    $("#slide-menu").swipe({
+//        //Generic swipe handler for all directions
+//        swipe: function (event, direction, distance, duration, fingerCount, fingerData) {
+//            //$(this).text("You swiped " + direction ); 
+//            console.log("You swiped " + direction);
+//            if (direction == "left")
+//            {
+//                closeMenu();
+//            }
+//
+//        }
+//    });
+//});
