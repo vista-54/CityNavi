5ddf3f0e035cac4233c99ca0b9c6f7ada638e495
vista <vista-54@ukr.net>
2015-08-22 22:56:45 +0300

    2256

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -55,6 +55,30 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification",
+        "clobbers": [
+            "cordova.plugins.notification.local",
+            "plugin.notification.local"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-core.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Core",
+        "clobbers": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
+    },
+    {
+        "file": "plugins/de.appplant.cordova.plugin.local-notification/www/local-notification-util.js",
+        "id": "de.appplant.cordova.plugin.local-notification.LocalNotification.Util",
+        "merges": [
+            "cordova.plugins.notification.local.core",
+            "plugin.notification.local.core"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -62,7 +86,9 @@ module.exports.metadata =
 {
     "com.evothings.ble": "1.0.0",
     "com.unarin.cordova.beacon": "3.3.0",
-    "cordova-plugin-device": "1.0.2-dev"
+    "cordova-plugin-device": "1.0.2-dev",
+    "de.appplant.cordova.plugin.local-notification": "0.8.2dev",
+    "android.support.v4": "21.0.1"
 }
 // BOTTOM OF METADATA
 });
